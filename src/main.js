772c43d3f64e62a4ae53d93aa312c70766ef9bc3
Sofia <sedv@kth.se>
2022-05-02 15:44:21 +0200

    function for writing out a letter in a box

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-
 // the looked for word
 var rightWord;
 
@@ -9,3 +8,15 @@ var guess3;
 var guess4;
 var guess5;
 var guess6;
+
+
+var boxes = document.getElementsByClassName('ruta');
+
+
+// function that writes out a letter in one of the boxes
+function writeOutLetter(index, inputBokstav){
+    var bokstav = document.createElement('H1');
+    bokstav.innerHTML = inputBokstav;
+    boxes[index].appendChild(bokstav);
+}
+
