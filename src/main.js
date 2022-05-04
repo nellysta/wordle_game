eff3ac75738d10a1aa9d65c8c2cdd3d327558753
Sofia <sedv@kth.se>
2022-05-04 21:54:35 +0200

    fixed so that a word's letters are correctly divided into an array

--- src/main.js
+++ src/main.js
@@ -16,28 +16,41 @@ rightWord = "ABCDE";
 let guessedWord = "ABCDF";
 const answers = [guess1,guess2,guess3,guess4,guess5,guess6];
 
-const rightWordArray = [];
-rightWordArrayn= rightWord.charAt(5);
+// Array with each letter of the right word as one element
+var rightWordArray = [];
+for (i=0;i<5;i++){
+    rightWordArray[i] = rightWord.charAt(i);
 
-let guessedWordArray = [];
-guessedWordArray= guessedWord.charAt(5);
+
+// Array with each letter of the guessed word as one element
+var guessedWordArray = [];
+for (i=0;i<5;i++){
+    guessedWordArray[i]= guessedWord.charAt(i);
+}
+
+
 
 let green = 0;
 let yellow = 0;
 let grey = 0;
 
+
+
+
 /**
  * This function compares word from user with correct word
  * @param guessedWordArray
  * @param rightWordArray
+ * @param guess         guess number where the first guess is 0
  */
 
-function checkLetters(guessedWordArray, rightWordArray) {
+function checkLetters(guessedWordArray, rightWordArray, guess) {
 
    for(let i = 0; i < guessedWordArray.length; i++ ){
        if(guessedWordArray[i] === rightWordArray[i]) { //current character in guessedWord is same as actual
            green ++;
-           console.log('green');
+           
+           boxes[guess*5 + i].setAttribute("style", "color=green");
        }
        else if(guessedWordArray.includes(rightWordArray[i])) //check if guess word includes correct letter
        {
@@ -52,6 +65,8 @@ function checkLetters(guessedWordArray, rightWordArray) {
 
 }
 
-function checkForYellowLetters2() {
 
-}
+// array of the boxes
+var boxes = document.getElementsByClassName("ruta");
+//function checkForYellowLetters2() {}
+document.getElementsByClassName("ruta").stye.backgroundColor = "pink";
