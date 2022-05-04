89a8871688d02dfd3b43be27906cbe3312293832
Nelly Ståhl <nellyst@kth.se>
2022-05-04 15:17:04 +0200

    Worked on checking status of letter (color)

--- src/main.js
+++ src/main.js
@@ -9,3 +9,49 @@ var guess3;
 var guess4;
 var guess5;
 var guess6;
+
+
+
+rightWord = "ABCDE";
+let guessedWord = "ABCDF";
+const answers = [guess1,guess2,guess3,guess4,guess5,guess6];
+
+const rightWordArray = [];
+rightWordArrayn= rightWord.charAt(5);
+
+let guessedWordArray = [];
+guessedWordArray= guessedWord.charAt(5);
+
+let green = 0;
+let yellow = 0;
+let grey = 0;
+
+/**
+ * This function compares word from user with correct word
+ * @param guessedWordArray
+ * @param rightWordArray
+ */
+
+function checkLetters(guessedWordArray, rightWordArray) {
+
+   for(let i = 0; i < guessedWordArray.length; i++ ){
+       if(guessedWordArray[i] === rightWordArray[i]) { //current character in guessedWord is same as actual
+           green ++;
+           console.log('green');
+       }
+       else if(guessedWordArray.includes(rightWordArray[i])) //check if guess word includes correct letter
+       {
+           yellow++;
+           console.log('yellow');
+       }
+       else{ //letter is not the same
+           grey++;
+           console.log('grey');
+       }
+   }
+
+}
+
+function checkForYellowLetters2() {
+
+}
