98813efb6d11a0f86f2d37a501934d00a51b8787
Sofia <sedv@kth.se>
2022-05-04 15:37:03 +0200

    write out input word

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 var rightWord;
 
 // save all guesses
-var guess1;
+// var guess1;
 var guess2;
 var guess3;
 var guess4;
@@ -10,6 +10,7 @@ var guess5;
 var guess6;
 
 
+// alla rutor i en array
 var boxes = document.getElementsByClassName('ruta');
 
 
@@ -19,4 +20,19 @@ function writeOutLetter(index, inputBokstav){
     bokstav.innerHTML = inputBokstav;
     boxes[index].appendChild(bokstav);
 }
+
+// fixa med onclick eller liknande
+
+var guess1 = document.getElementById("guess1").value;
+
+
+
+
+// skriv ut alla bokstäver i gissning.
+function writeOutAWord1(){
+    for (i=0;i<5;i++){
+        writeOutLetter(i, guess1.charAt(i));
+    }
+}
+
 
