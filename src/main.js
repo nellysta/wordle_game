b31f1dfc2a59ce51e5b484cd240d8f59e0a9da57
Sofia <sedv@kth.se>
2022-05-05 21:00:06 +0200

    user can only write their guesses in correct order

--- src/main.js
+++ src/main.js
@@ -10,6 +10,9 @@ var guess4;
 var guess5;
 var guess6;
 
+var guessCounter = 0;
+
+
 
 // alla rutor i en array
 var boxes = document.getElementsByClassName('ruta');
@@ -22,45 +25,39 @@ function writeOutLetter(row, index, inputBokstav){
     boxes[5*row + index].appendChild(bokstav);
 }
 
-
-// the following 6 methods are being called after user pushes 
-// submit button after each guess. The method takes in the guess
-// and sends it to the method 'writeOutWord'
-function writeOutAWord1(){
-    guess1 = document.getElementById("guess1").value;
-    writeOutAWord(guess1, 0);
-}
-
-function writeOutAWord2(){
-    guess2 = document.getElementById("guess2").value;
-    writeOutAWord(guess2, 1);
-}
-
-function writeOutAWord3(){
-    guess3 = document.getElementById("guess3").value;
-    writeOutAWord(guess3, 2);
-}
-
-function writeOutAWord4(){
-    guess4 = document.getElementById("guess4").value;
-    writeOutAWord(guess4, 3);
-}
-
-function writeOutAWord5(){
-    guess5 = document.getElementById("guess5").value;
-    writeOutAWord(guess5, 4);
-}
-
-function writeOutAWord6(){
-    guess6 = document.getElementById("guess6").value;
-    writeOutAWord(guess6, 5);
-}
-
 // skriv ut alla bokstäver i gissning.
 function writeOutAWord(guess, row){
     for (i=0;i<5;i++){
         writeOutLetter(row, i, guess.charAt(i));
     }
 }
+
+
+// wait for guess to be sumbitted
+document.getElementById("knapp").onclick = function(){
+    var guess;
+    if (guessCounter === 0){
+        guess1 = document.getElementById("guess").value;
+        guess = guess1;
+    } else if (guessCounter === 1){
+        guess2 = document.getElementById("guess").value;
+        guess = guess2;
+    } else if (guessCounter === 2){
+        guess3 = document.getElementById("guess").value;
+        guess = guess3;
+    } else if (guessCounter === 3){
+        guess4 = document.getElementById("guess").value;
+        guess = guess4;
+    } else if (guessCounter === 4){
+        guess5 = document.getElementById("guess").value;
+        guess = guess5;
+    } else if (guessCounter === 5){
+        guess6 = document.getElementById("guess").value;
+        guess = guess6;
+    } 
+    writeOutAWord(guess,guessCounter);
+    guessCounter++;
+};
+
 
 
