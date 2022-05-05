61f250bc88fe41d6c7e97f385d0f518cdc852681
Sofia <sedv@kth.se>
2022-05-05 21:39:06 +0200

    fixed checkLetters method so that it changes colors of boxes

--- src/main.js
+++ src/main.js
@@ -10,23 +10,27 @@ var guess4;
 var guess5;
 var guess6;
 
+var guessCount = 0;
+
 
 
 rightWord = "ABCDE";
 let guessedWord = "ABCDF";
 const answers = [guess1,guess2,guess3,guess4,guess5,guess6];
 
+
 // Array with each letter of the right word as one element
-var rightWordArray = [];
+var rightWordArray = [5];
 for (i=0;i<5;i++){
     rightWordArray[i] = rightWord.charAt(i);
 
-
+}
 // Array with each letter of the guessed word as one element
-var guessedWordArray = [];
+var guessedWordArray = [5];
 for (i=0;i<5;i++){
     guessedWordArray[i]= guessedWord.charAt(i);
 }
+
 
 
 
@@ -35,7 +39,11 @@ let yellow = 0;
 let grey = 0;
 
 
+// array of the boxes
+var boxes = document.getElementsByClassName("ruta");
 
+// test for checkLetters function
+checkLetters(guessedWordArray,rightWordArray);
 
 /**
  * This function compares word from user with correct word
@@ -44,29 +52,28 @@ let grey = 0;
  * @param guess         guess number where the first guess is 0
  */
 
-function checkLetters(guessedWordArray, rightWordArray, guess) {
-
+function checkLetters(guessedWordArray, rightWordArray) {
    for(let i = 0; i < guessedWordArray.length; i++ ){
+       var box = boxes[5*guessCount + i];
        if(guessedWordArray[i] === rightWordArray[i]) { //current character in guessedWord is same as actual
-           green ++;
+           green++;
            
-           boxes[guess*5 + i].setAttribute("style", "color=green");
-       }
-       else if(guessedWordArray.includes(rightWordArray[i])) //check if guess word includes correct letter
-       {
+           box.style.backgroundColor = "green";    // change color of box to green
+        }   else if(guessedWordArray.includes(rightWordArray[i])) { //check if guess word includes correct letter
            yellow++;
-           console.log('yellow');
-       }
-       else{ //letter is not the same
+           box.style.backgroundColor = "yellow"; // change color of box to yellow
+        }   else{ //letter is not the same
            grey++;
-           console.log('grey');
-       }
-   }
+           box.style.backgroundColor = "red"; // change color of box to red
+        }
+    }
 
 }
 
 
-// array of the boxes
-var boxes = document.getElementsByClassName("ruta");
-//function checkForYellowLetters2() {}
-document.getElementsByClassName("ruta").stye.backgroundColor = "pink";
+
+
+
+
+
+// function checkForYellowLetters2() {}
